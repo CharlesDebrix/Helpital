4aee690a87c0498c192175285d490d3eab954094
Charles Debrix <charles.debrix@epitech.eu>
2020-09-07 17:57:29 +0200

    [+] first page impression [+]

---
+++ app.js
@@ -0,0 +1,20 @@
+// HELPITAL //
+
+//Test
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('<!DOCTYPE html>'+
+'<html>'+
+'    <head>'+
+'        <meta charset="utf-8" />'+
+'        <title>Ma page Node.js !</title>'+
+'    </head>'+ 
+'    <body>'+
+'     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
+'    </body>'+
+'</html>');
+    res.end();
+});
+server.listen(8080);
