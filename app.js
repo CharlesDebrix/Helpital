51f65d49f629efad27be9b81e427cddf0628a89d
Charles Debrix <charles.debrix@epitech.eu>
2020-09-08 17:49:29 +0200

    [+] news front features [+]

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 // HELPITAL //
 
 const http = require('http');
+var url = require('url');
 const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = 8080;
 
 app.use(express.json());
 app.use(express.static(__dirname+"/public"));
 
+app.listen(port);
+
+app.get('/agent', function(req, res) {
+    console.log('agent page requested');
+    res.sendFile(path.join(__dirname+'/public/src/views/agent.html'));
+});
+
+app.get('/chief', function(req, res) {
+    console.log('chief page requested');
+    res.sendFile(path.join(__dirname+'/public/src/views/chief.html'));
+});
+
 // default URL for website
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
 });
-
-const server = http.createServer(app);
-const port = 8080;
-server.listen(port);
-console.debug('Server listening on port ' + port);
