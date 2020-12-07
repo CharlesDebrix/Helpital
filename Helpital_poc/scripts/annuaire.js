d658202d9b25bb903cf73228e57898d4e2961aef
Charles Debrix <charles.debrix@epitech.eu>
2020-12-07 16:48:57 +0100

    [+] Annuaire done with json files [+]

--- Helpital_poc/scripts/annuaire.js
+++ Helpital_poc/scripts/annuaire.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 function addRow() {
     var myName = document.getElementById("name");
     var email = document.getElementById("email");
@@ -26,4 +28,21 @@ function deleteRow(obj) {
 
 function load() {
     console.log("Page load finished");
+
+    const file = "../annuaire.json";
+    var request = new XMLHttpRequest();
+    request.open("GET", file, false);
+    request.send(null)
+    var my_JSON_object = JSON.parse(request.responseText);
+    
+    for (var i = 0; i < my_JSON_object.length; i++) {
+        var table = document.getElementById("myTableData");
+        var rowCount = table.rows.length;
+        var row = table.insertRow(rowCount);
+
+        row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+        row.insertCell(1).innerHTML= my_JSON_object[i].name;
+        row.insertCell(2).innerHTML= my_JSON_object[i].email;
+        row.insertCell(3).innerHTML= my_JSON_object[i].phone;
+    }
 }
