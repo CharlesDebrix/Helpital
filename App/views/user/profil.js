84e3b9a0fa09557ef9701ce517984b71ed8cf9c6
VHouze <victor.houze@epitech.eu>
2020-12-11 02:34:29 +0100

    {+} Back should be finished for profil, need someone to implement front{+}

--- App/views/user/profil.js
+++ App/views/user/profil.js
@@ -1,14 +1,45 @@
-// let users = [];
+const fs = require('fs');
 
-// fs.readFile('./views/users.json', (err, data) => {
-//     if (err) throw err;
-//     users = JSON.parse(data);
-// });
+let users = [];
 
-// let name = "";
-// let surname = "";
-// let email = "";
-// let photo = "";
-// let telephone = "";
-// let dateDeNaissance = "";
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return;
+}
 
+
+
+function init() {
+  
+    fs.readFile('views/users.json', (err, data) => {
+        console.log("il passe par le readfile");
+        users = JSON.parse(data);
+        addElement();
+    })
+}
+
+function addElement() {
+    let cookieId = getCookie('Id');
+    const user = users.find(u => {
+        return u.id === cookieId;
+    });
+    let name = document.getElementById('name'); 
+    name.insertAdjacentHTML('afterend', `<h2> ${user.name} </h2>`);
+    let surname = document.getElementById('surname'); 
+    surname.insertAdjacentHTML('afterend', `<h2> ${user.surname} </h2>`);
+    let email = document.getElementById('email'); 
+    email.insertAdjacentHTML('afterend', `<h2> ${user.email} </h2>`);
+    let phone = document.getElementById('phone'); 
+    phone.insertAdjacentHTML('afterend', `<h2> ${user.phone} </h2>`);
+}
