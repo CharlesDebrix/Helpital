c4ecd4b8aa4800641cda3e7b58bd9ce7a2c063c1
Manu <emmanuel.lena@epitech.eu>
2020-12-11 14:31:44 +0100

    {~} patching rooms {~}

--- App/views/chambres/availability.js
+++ App/views/chambres/availability.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
 
+let roomAvailability;
+
 function init() {
-    createRooms()
+    fs.readFile('views/database/room_availability.json', (err, data) => {
+        roomAvailability = JSON.parse(data);
+        createRooms()
+    })
 }
 
 function readSingleFile(e) {
@@ -10,131 +15,42 @@ function readSingleFile(e) {
             console.log("File read failed:", err)
             return
         }
-        return jsonString
     })
 }
 
-function createRoom(room, htmlRoom) {
+function createRoom(room) {
     let divRoomNumber = document.createElement('div')
-    let divRoomDevelopArrow = document.createElement('div')
-    let spanRoomNumber = document.createElement('span')
-    let iRoomArrow = document.createElement('i')
 
-    spanRoomNumber.setAttribute('class', "room-title")
+    divRoomNumber.setAttribute("class", "n_room")
 
-    spanRoomNumber.innerHTML += `${room.number}`
-    divRoomNumber.appendChild(spanRoomNumber)
-
-    divRoomNumber.setAttribute('class', 'room-title-div')
-    divRoomDevelopArrow.setAttribute('class', 'room-arrow')
-    iRoomArrow.setAttribute('class', 'material-icons')
-    iRoomArrow.innerHTML = "keyboard_arrow_down"
-    iRoomArrow.onclick = function (event) {
-        if (iRoomArrow.innerHTML === "keyboard_arrow_down") {
-            iRoomArrow.innerHTML = "keyboard_arrow_up"
-            event.target.parentNode.parentNode.setAttribute('class', 'room-expanded')
-            event.target.parentNode.parentNode.scrollIntoView()
+    divRoomNumber.innerHTML = `${room.number}`
+    divRoomNumber.onclick = function () {
+        if (divRoomNumber.getAttribute("class") === "n_room-active") {
+            divRoomNumber.setAttribute("class", "n_room")
+            divRoomNumber.innerHTML = `${room.number}`
         } else {
-            iRoomArrow.innerHTML = "keyboard_arrow_down"
-            event.target.parentNode.parentNode.setAttribute('class', 'room')
-            event.target.parentNode.parentNode.scrollIntoView()
+            divRoomNumber.setAttribute("class", "n_room-active")
+            if (room.bed_available === 1) {
+                divRoomNumber.innerHTML += `<br><br>Un seul lit disponible<br><br>Capacité de la chambre : ${room.capacity}`
+            } else if (room.bed_available === 0) {
+                divRoomNumber.innerHTML += `<br><br>Aucun lit disponible<br><br>Capacité de la chambre : ${room.capacity}`
+            } else {
+                divRoomNumber.innerHTML += `<br><br>Lits disponibles : ${room.bed_available}<br><br>Capacité de la chambre : ${room.capacity}`
+            }
         }
-        console.log()
     }
-    divRoomDevelopArrow.appendChild(iRoomArrow)
-    /*    if (room.bed_available === 0)
-            divRoomNumber.innerHTML += "There is no bed available in this room<br>"
-        else if (room.bed_available === 1)
-            divRoomNumber.innerHTML += `There is ${room.bed_available} bed available in this room<br>`
-        else
-            divRoomNumber.innerHTML += `There are ${room.bed_available} beds available in this room<br>`
-
-        room.beds.forEach(function (bed) {
-            if (bed.availability === "vacant")
-                divRoomNumber.innerHTML += `The bed ${bed.number} is empty<br>`
-            else
-                divRoomNumber.innerHTML += `The bed ${bed.number} is taken<br>`
-        })*/
-    htmlRoom.appendChild(divRoomNumber)
-    htmlRoom.appendChild(divRoomDevelopArrow)
+    return divRoomNumber
 }
 
 function createRooms() {
-    let element = document.getElementById('rooms_list')
+    let id = document.getElementById('rooms_list')
+    let divNoSelect = document.createElement('div');
+
+    divNoSelect.setAttribute('class', 'noselect')
+
+    id.appendChild(divNoSelect)
 
     roomAvailability.rooms.forEach(function (room) {
-        let li = document.createElement('div');
-        li.setAttribute('class', 'room')
-        createRoom(room, li)
-        element.appendChild(li);
+        divNoSelect.appendChild(createRoom(room))
     });
-}
-
-const roomAvailability = {
-    "rooms": [
-        {
-            "number": 301,
-            "capacity": 2,
-            "taken": 1,
-            "bed_available": 1,
-            "beds": [
-                {
-                    "number": 0,
-                    "availability": "vacant"
-                },
-                {
-                    "number": 1,
-                    "availability": "taken"
-                }
-            ]
-        },
-        {
-            "number": 302,
-            "capacity": 1,
-            "taken": 1,
-            "bed_available": 0,
-            "beds": [
-                {
-                    "number": 0,
-                    "availability": "taken"
-                }
-            ]
-        },
-        {
-            "number": 303,
-            "capacity": 1,
-            "taken": 0,
-            "bed_available": 1,
-            "beds": [
-                {
-                    "number": 0,
-                    "availability": "vacant"
-                }
-            ]
-        },
-        {
-            "number": 304,
-            "capacity": 4,
-            "taken": 1,
-            "bed_available": 3,
-            "beds": [
-                {
-                    "number": 0,
-                    "availability": "vacant"
-                },
-                {
-                    "number": 1,
-                    "availability": "vacant"
-                },
-                {
-                    "number": 2,
-                    "availability": "vacant"
-                },
-                {
-                    "number": 3,
-                    "availability": "taken"
-                }
-            ]
-        }
-    ]
 }
